4a99c8b83f865baa6cf657a44c8d4126507e248f
wilmerxx <wmshagnay@espe.edu.ec>
2024-08-21 05:44:14 -0500

    cambios de configuracion

--- run.py
+++ run.py
@@ -8,6 +8,9 @@ def main():
     # Ejecutar la aplicación en modo de desarrollo
     app.run(debug=True)
 
+    # Ejecutar la aplicación en modo de desarrollo
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
 
 if __name__ == "__main__":
     main()
