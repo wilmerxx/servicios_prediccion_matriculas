00c1f7bda0521a2f14d88508c51e2721eae614f6
wilmerxx <wmshagnay@espe.edu.ec>
2024-08-25 18:56:27 -0500

    cambios en la procfile 5

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 from app import create_app
 import os
 
-def main():
-    # Crear la instancia de la aplicacións
-    app = create_app()
-
-    # Ejecutar la aplicación en modo de desarrollo
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
-
+# Crear la instancia de la aplicación
+app = create_app()
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
